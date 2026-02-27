62055d7fc0631bc6e7e3cc60988166cf1b463bc7
Rawan Hussein Mohamed <rawanhussien75@email.com>
2026-02-27 22:49:23 +0200

    عملته واجهة إنشاء الحساب

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from .config import Config
 from .extensions import mysql, jwt
 from .routes.auth import auth_bp
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
     app.config.from_object(Config)
 
     # initialize extensions
@@ -12,6 +14,6 @@ def create_app():
     jwt.init_app(app)
 
     # register blueprints
-    app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(auth_bp, url_prefix="/api//auth")
 
     return app
--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ class Config:
 
     SECRET_KEY = 'a9f823jkf93n2f0sdfn2398h23hfsd923hsdf923'
     # إعدادات الاتصال بقاعدة البيانات
-    MYSQL_HOST = 'localhost'       # أو IP السيرفر لو سيرفر خارجي
+    MYSQL_HOST = '127.0.0.1'       # أو IP السيرفر لو سيرفر خارجي
     MYSQL_USER = 'root'            # اسم المستخدم بتاعك
     MYSQL_PASSWORD = ''            # الباسورد بتاع MySQL
     MYSQL_DB = 'face_recognition_db'     # اسم قاعدة البيانات
