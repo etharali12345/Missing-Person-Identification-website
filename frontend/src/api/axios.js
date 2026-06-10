f9b6cc01d01db2cca036b209ee82a7194f51739b
etharali12345 <mayadaethar@gmail.com>
2026-06-10 17:30:57 +0300

    "axios"

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://127.0.0.1:5000";
+//export const BASE_URL = "http://127.0.0.1:5000";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
 
 export const api = axios.create({
   baseURL: `${BASE_URL}/api`,
